e49019fde0757a413cea5843d1614e93c19c97d6
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-16 15:45:56 +0000

    Initial Dev Enviroment
    Project Starts Here

---
+++ Source/The Hag/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public CharacterController characterController;
+
+    [System.NonSerialized]
+    public float speed = 5f;
+    [System.NonSerialized]
+    public float gravity = -15f;
+    [System.NonSerialized]
+    public float jumpHeight = 2f;
+
+    Vector3 moveVelocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+    bool isGrounded;
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        moveVelocity = transform.right * x + transform.forward * z;
+
+        characterController.Move(moveVelocity * speed * Time.deltaTime);
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (isGrounded && moveVelocity.magnitude > 0.6f && !GetComponent<AudioSource>().isPlaying)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
