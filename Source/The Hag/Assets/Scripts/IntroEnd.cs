d772d3a6c4af9e64506021c3aed8e2acc98bea07
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-21 05:04:46 +0000

    Added Intro and Menu Scenes

---
+++ Source/The Hag/Assets/Scripts/IntroEnd.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroEnd : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+
+    public void endIntro()
+    {
+        sceneLoader.LoadNextScene();
+    }
+}
---
+++ Source/The Hag/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public Animator transition;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Submit") || Input.GetButton("Jump"))
+        {
+            LoadNextScene();
+        }
+    }
+
+    public void LoadNextScene()
+    {
+       StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    IEnumerator LoadLevel(int index)
+    {
+        transition.SetTrigger("EndFade");
+
+        yield return new WaitForSeconds(3f);
+
+        SceneManager.LoadScene(index);
+    }
+}
