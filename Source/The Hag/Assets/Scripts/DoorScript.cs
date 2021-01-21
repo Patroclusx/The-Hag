632a1eeb4380a7206b7bb20b9018e7eeb0a0e69c
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-21 23:39:29 +0000

    Updated Animations
    -House model updates

---
+++ Source/The Hag/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    public Animator transition;
+    RaycastHit hitInfo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 1.1f))
+        {
+            if (Input.GetButtonDown("Fire1"))
+            {
+                if ((transition.GetCurrentAnimatorStateInfo(0).IsName("DefaultState") && transition.GetBool("isClosedByDefault")) || !transition.GetCurrentAnimatorStateInfo(0).IsName("DoorOpen"))
+                {
+                    transition.SetTrigger("setOpen");
+                }
+                else
+                {
+                    transition.SetTrigger("setClose");
+                }
+            }
+        }
+    }
+}
