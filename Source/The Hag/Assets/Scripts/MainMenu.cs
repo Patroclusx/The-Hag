5634c91ade09d52e206c92f908f633fc74eb1793
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-21 13:46:30 +0000

    Added Basic Main Menu

---
+++ Source/The Hag/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    public SceneLoader sceneLoader;
+    public AudioSource musicSource;
+
+    public void startGame()
+    {
+        musicSource.Stop();
+        sceneLoader.LoadNextScene();
+    }
+}
