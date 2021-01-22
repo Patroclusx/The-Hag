b979236931296d1d614e66d65ebc8a6916c851ad
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-22 11:53:17 +0000

    Updated Menu Scene

--- Source/The Hag/Assets/Scripts/MainMenu.cs
+++ Source/The Hag/Assets/Scripts/MainMenu.cs
@@ -5,12 +5,25 @@ using UnityEngine;
 public class MainMenu : MonoBehaviour
 {
     public SceneLoader sceneLoader;
-    public Camera mainCamera;
+    public Camera menuCamera;
+    public Canvas menuCanvas;
 
     public void startGame()
     {
-
+        menuCanvas.enabled = false;
+        StartCoroutine(ModifyFov(8f));
         FindObjectOfType<AudioManager>().playSound("StartGame");
         sceneLoader.LoadNextScene();
     }
+
+    IEnumerator ModifyFov(float fovSpeed)
+    {
+         float defaultFov = menuCamera.fieldOfView;;
+         while (true)
+         {
+             menuCamera.fieldOfView -= defaultFov * Time.deltaTime / fovSpeed;
+
+             yield return null;
+         }
+     }
 }
--- Source/The Hag/Assets/Scripts/SceneLoader.cs
+++ Source/The Hag/Assets/Scripts/SceneLoader.cs
@@ -21,24 +21,9 @@ public class SceneLoader : MonoBehaviour
 
     public void LoadNextScene()
     {
-        //StartCoroutine(ModifyFov());
         StartCoroutine(AudioFade.FadeOut(musicSource, 2.5f));
         StartCoroutine(LoadLevel(SceneManager.GetActiveScene().buildIndex + 1));
     }
-   /** IEnumerator ModifyFov()
-    {
-        Camera menuCamera = FindObjectOfType<Camera>();
-
-        float defaultFov = menuCamera.fieldOfView;;
-        while (Camera.current.fieldOfView < 180)
-        {
-            menuCamera.fieldOfView -= defaultFov * Time.deltaTime / 4f;
-
-            yield return null;
-        }
-
-        menuCamera.fieldOfView = defaultFov;
-    }**/
 
     IEnumerator LoadLevel(int index)
     {
