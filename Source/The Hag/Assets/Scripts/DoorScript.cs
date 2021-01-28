bf2f3af8a4a73c845501891509989ed1f3bca39c
Patroclus <Patroclus@DESKTOP-OIMNU28>
2021-01-28 18:04:55 +0000

    Added PlayerStats and player sound effects
    -Added player status container
    -Added player stamina to PlayerStats
    -Added player sound effects (Crouch, Breath, Jump)
    -Updated FrequencyTimer to use prefixes

--- Source/The Hag/Assets/Scripts/DoorScript.cs
+++ Source/The Hag/Assets/Scripts/DoorScript.cs
@@ -14,7 +14,7 @@ public class DoorScript : MonoBehaviour
     {
         if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 1.25f, doorMask))
         {
-            if (Input.GetButtonDown("Fire1") && hitInfo.transform.gameObject.Equals(doorObject))
+            if (Input.GetButtonDown("Interact") && hitInfo.transform.gameObject.Equals(doorObject))
             {
                 if (transition.GetCurrentAnimatorStateInfo(0).IsName("DefaultState") || transition.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f){
                     transition.SetBool("isOpened", !transition.GetBool("isOpened"));
