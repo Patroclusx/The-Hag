533565dab6b11522b1715e64a7766d8f4100877a
István Balázs Táborosi <55479420+Ultimateees@users.noreply.github.com>
2021-02-01 17:53:49 +0100

    Hand&Lantern Models
    Hand Model V1
    Lantern Model V1

--- Source/The Hag/Assets/Scripts/DoorScript.cs
+++ Source/The Hag/Assets/Scripts/DoorScript.cs
@@ -1,14 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class DoorScript : MonoBehaviour
 {
     public Animator transition;
     public GameObject doorObject;
     public LayerMask doorMask;
+    public PointerEventData eventData;
     RaycastHit hitInfo;
-
+   /* public virtual void OnPointerClick(PointerEventData eventData)
+    {
+        if (eventData.clickCount == 2)
+        {
+            Debug.Log("double click");
+        }
+    }*/
     // Update is called once per frame
     void Update()
     {
@@ -18,7 +26,20 @@ public class DoorScript : MonoBehaviour
             {
                 if (transition.GetCurrentAnimatorStateInfo(0).IsName("DefaultState") || transition.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f){
                     transition.SetBool("isOpened", !transition.GetBool("isOpened"));
+                } 
+            }
+            else if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hitInfo, 1.25f, doorMask) && eventData.clickCount == 2)
+            {
+                Debug.Log("double click");
+               
+                if (Input.GetButtonDown("Interact") && hitInfo.transform.gameObject.Equals(doorObject))
+                {
+                    if (transition.GetCurrentAnimatorStateInfo(0).IsName("DefaultState") || transition.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1f)
+                    {
+                        transition.SetBool("isOpened", !transition.GetBool("isOpened"));
+                    }
                 }
+
             }
         }
     }
